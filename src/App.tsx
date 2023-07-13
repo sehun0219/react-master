1bef35df3c9f5bbe7e12c74e86e08294c22e3968
Cayde Kim <cloudnine0219@gmail.com>
2023-07-14 01:12:47 +1200

    change to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import styled from "styled-components"; // import styled-components
-
 const Title = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
-
-
 const Wrapper = styled.div`
   display: flex;
-  height: 100vh;
   width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
   background-color: ${(props) => props.theme.backgroundColor};
 `;
-
 
 function App() {
   return (
