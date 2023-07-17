7380be2e1f3e171851975d4b757c84cbb7555a54
Cayde Kim <cloudnine0219@gmail.com>
2023-07-17 23:56:46 +1200

    Nested Routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-
-const darkTheme = {
-  textColor: "whiteSmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whiteSmoke",
-};
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
